51285317711f76caf04b3b46cf27366e1d10501f
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-08-28 00:10:05 +0000

    [Create] 新規登録機能とcreate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
+  end
+  
   def index
   end
 
@@ -10,4 +17,9 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end  
 end
