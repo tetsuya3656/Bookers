a18e91282f726cbf61971bffe70ee8018512de7f
tetsuya <110829351+tetsuya3656@users.noreply.github.com>
2022-08-31 12:08:58 +0000

    [Create] update,destroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
@@ -11,6 +8,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -26,12 +24,12 @@ class BooksController < ApplicationController
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
-  end  
+  end
 
   private
   def book_params
